7a42e986e2a263c2e57aea67d10f7f22ba426c7e
Surya626 <92287638+Surya626@users.noreply.github.com>
2021-10-27 10:25:35 +0530

    Add files via upload

---
+++ Untitlwertwered1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<limits.h>
+void main()
+{
+	printf("short int:\nsigned: %hd to %hd\n",SHRT_MIN,SHRT_MAX);
+	printf("unsigned-%d to %hu\n",0,USHRT_MAX);
+	printf("int:\nsigned: %d to %d\n",INT_MIN,INT_MAX);
+	printf("unsigned: %d to %u\n",0,INT_MAX);
+	printf("long int:\nsigned: %ld to %ld\n",LONG_MIN,LONG_MAX);
+	printf("unsigned: %ld to %lu\n",0,LONG_MAX);
+	printf("long long int:\nsigned: %lld to %lld\n",LONG_LONG_MIN,LONG_LONG_MAX);
+	printf("unsigned: %d to %llu",0,LONG_LONG_MAX);
+	
+}
