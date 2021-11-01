33339c7757bc5695a886a1e5c8350fc50c1a86fa
Surya626 <92287638+Surya626@users.noreply.github.com>
2021-11-01 10:56:03 +0530

    Add files via upload

---
+++ naturals_sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int n,s,a,b;
+	n=5;
+	s=n*(n+1)/2;
+	a=10;
+	b=a*(a+1)/2;
+	
+	printf("sum of %d natural numbers is %d\n",n,s);
+	printf("sum of %d natural numbers is %d",a,b);
+}
