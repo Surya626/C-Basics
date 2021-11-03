ff7fc43297bf2621ccdc9af65bce53f1d3a3bf12
Surya626 <92287638+Surya626@users.noreply.github.com>
2021-11-03 10:18:57 +0530

    Add files via upload

---
+++ minutes to hours.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int m,h;
+	m=100/60;
+	h=100%60;
+	printf("%d hours %d minutes\n",m,h);
+	int min;
+	min=550;
+	printf("%d hours %d minutes",min/60,min%60);
+	
+
+	
+	
+}
