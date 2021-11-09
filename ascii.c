e9071853f553effbff3f415c5a71d72d1927e6e0
Surya626 <92287638+Surya626@users.noreply.github.com>
2021-11-09 10:09:06 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main ()
+{
+	int a;
+	a=67;
+	printf("corresponding ascii charcter for %d is %c",a,a);
+}
---
+++ ascii2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main ()
+{
+	char ch;
+	ch='Z';
+	printf("corresponding ASCII number for %c is %d",ch,ch);
+}
