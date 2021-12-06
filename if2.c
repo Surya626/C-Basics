3477fcfa2fb27bc01a8dede1ae95956d455764af
Surya626 <92287638+Surya626@users.noreply.github.com>
2021-12-06 10:43:46 +0530

    Add files via upload

---
+++ if2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	
+	if(n>0)
+	{
+		printf("the number %d is positve",n);
+	}
+	else
+	{
+		printf("the number %d is negative",n);
+	}
+}
---
+++ vote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	printf("enter your age:");
+	scanf("%d",&age);
+	
+	if(age>18)
+	{
+		printf("you can vote",age);
+	}
+	else
+	{
+		printf("orey kurra nekku votr veyadaniki enka time undhi le poo",age);
+	}
+}
