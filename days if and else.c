08330d5b317de473feead149cd71ed5bc1c02525
Surya626 <92287638+Surya626@users.noreply.github.com>
2021-12-07 09:55:07 +0530

    Add files via upload

---
+++ days if and else.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	
+	if(n==1)
+	{
+		printf("monday");   
+		}
+		
+	else if(n==2)
+	{
+		printf("tuesday");   
+		}
+		
+	else if(n==3)
+	{
+		printf("wednesday");   
+		}
+		
+	else if(n==4)
+	{
+		printf("thrusday");   
+		}
+		
+	else if(n==5)
+	{
+		printf("friday");   
+		}
+		
+	else if(n==6)
+	{
+		printf("saturday");   
+		}
+		
+	else if(n==7)
+	{
+		printf("sunday");   
+		}
+		
+	else if(n>7)
+	{
+		printf("invalid option");   
+		}		
+}
